c1460faeb8b1d237bf163bf22a519c5ab24ecc09
Ljupch0 <46574875+Ljupch0@users.noreply.github.com>
2019-01-31 23:14:08 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ function unhighlight (btn) {
   btn.style.backgroundColor = "#696969";
 }
 
+const kick = new Audio('TR808all/TR808WAV/BD/BD0000.wav');
+const playKick = () => {
+  kick.play()
+}
+
+
 buttons.forEach((button) => {
 	button.addEventListener('mouseover', () => {
   	highlight(button);
@@ -16,3 +22,10 @@ buttons.forEach((button) => {
   	unhighlight(button);
   });
 });
+
+/* for (i=1; i<10; i++) {
+  let buttoni=document.getElementById('i');
+} */
+
+const button1=document.getElementById('1')
+button1.addEventListener('mousedown', playKick)
